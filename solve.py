b7f54d78fec92edd3b4485d563cb17eddcf9902f
Eric Woroshow <eric@ericw.ca>
2021-02-01 15:38:52 -0800

    Another minor solving fix

--- solve.py
+++ solve.py
@@ -10,6 +10,11 @@ import segmentation
 
 
 class Card:
+    COUNTS = {
+        1: 0,
+        2: 1,
+        3: 2,
+    }
     FILLS = {
         "solid": 0,
         "outline": 1,
@@ -31,23 +36,24 @@ class Card:
         fill = classified["fill"]
         color = classified["color"]
         shape = classified["shape"]
-        self.attrs = (count, Card.FILLS[fill], Card.COLORS[color], Card.SHAPES[shape])
-        self.label = f"{count}-{fill}-{color}-{shape}"
+        self.attrs = (Card.COUNTS[count], Card.FILLS[fill], Card.COLORS[color], Card.SHAPES[shape])
+        self.label = f"{count}-{fill}-{color}-{shape}-{id}"
         self.rect = rect
 
     @staticmethod
     def find_sets(cards):
         def id(attrs):
             return (attrs[0] << 6) | (attrs[1] << 4) | (attrs[2] << 2) | (attrs[3] << 0)
-        cards_by_id = { id(c.attrs): c for c in cards }
+        seen_by_id = {}
 
         sets = []
         for i, card_i in enumerate(cards):
             for j, card_j in enumerate(cards[i+1:], i+1):
                 third_attrs = [-(a_i + a_j) % 3 for (a_i, a_j) in zip(card_i.attrs, card_j.attrs)]
-                third_card = cards_by_id.get(id(third_attrs))
-                if third_card:
-                    sets.append((card_i, card_j, third_card))
+                card_k = seen_by_id.get(id(third_attrs))
+                if card_k:
+                    sets.append((card_i, card_j, card_k))
+            seen_by_id[id(card_i.attrs)] = card_i
         return sets
 
 SET_COLORS_BGR = [
@@ -56,6 +62,7 @@ SET_COLORS_BGR = [
     [139, 167, 242],
     [214, 139, 242],
     [139, 219, 242],
+    [222, 247, 134],
 ]
 
 def expand_rect(rect, amount):
@@ -97,7 +104,7 @@ def main():
     for i, set_i in enumerate(sets):
         for card in set_i:
             labels = label_counts.get(card.attrs, 0)
-            cv2.polylines(img, [expand_rect(card.rect, labels * OUTLINE_WIDTH)], True, SET_COLORS_BGR[i%4], OUTLINE_WIDTH)
+            cv2.polylines(img, [expand_rect(card.rect, labels * OUTLINE_WIDTH)], True, SET_COLORS_BGR[i], OUTLINE_WIDTH)
             label_counts[card.attrs] = labels + 1
 
     cv2.imwrite(f"{sys.argv[2]}.png", img)
